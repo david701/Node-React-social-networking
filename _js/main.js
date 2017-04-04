7c8e2c5349594bce361f8cad85d555ca42709cd5
webdev498 <gcmobile@mail.com>
2017-04-04 14:51:53 -0500

    initial commit

--- _js/main.js
+++ _js/main.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './components/scripts.js';
+
 import '../_sass/main.scss';
 
-class Example extends React.Component{
-	render(){
-		return(
-			<h1>Example component</h1>
-		)
-	}
-}
-
-if(document.getElementById('example'))
-	ReactDOM.render(<Example />, document.getElementById('example'))
+// class Example extends React.Component{
+// 	render(){
+// 		return(
+// 			<h1>Example component</h1>
+// 		)
+// 	}
+// }
+//
+// if(document.getElementById('example'))
+// 	ReactDOM.render(<Example />, document.getElementById('example'))
--- _routes/static.js
+++ _routes/static.js
@@ -2,7 +2,35 @@ var express = require('express'),
 		router = express.Router();
 
 router.get('/', (req, res)=>{
-	res.render('home', {title: 'Home'})
+	res.render('home', {title: ''})
+});
+
+router.get('/login', (req, res)=>{
+	res.render('login', {title: 'Login'})
+});
+
+router.get('/signup', (req, res)=>{
+	res.render('signup', {title: 'Sign Up'})
+});
+
+router.get('/author', (req, res)=>{
+	res.render('author', {title: 'Author Page'})
+});
+
+router.get('/dashboard', (req, res)=>{
+	res.render('dashboard', {title: 'Dashboard'})
+});
+
+router.get('/dashboard/create', (req, res)=>{
+	res.render('dashboard-create', {title: 'Create'})
+});
+
+router.get('/forum', (req, res)=>{
+	res.render('forum', {title: 'Forum'})
+});
+
+router.get('/search', (req, res)=>{
+	res.render('search', {title: 'Search'})
 });
 
 module.exports = router;
