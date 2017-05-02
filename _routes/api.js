82c4d80fa5e6662c4339de3f1532900761be9342
webdev498 <gcmobile@mail.com>
2017-05-02 15:31:29 -0500

    user book info

--- _routes/api.js
+++ _routes/api.js
@@ -43,7 +43,7 @@ router.route('/books/:id')
 	.put(Books.editBook)
 	.delete(Books.removeBook)
 
-router.route('user/:id/books/')
+router.route('/users/:id/books/')
 	.get(Books.getUserBooks)
 
 /// END BOOKS API ///
