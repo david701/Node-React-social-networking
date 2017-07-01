0e8e8e8b696fecb88a9a7e1125e326e39112cafc
webdev498 <gcmobile@mail.com>
2017-07-01 16:54:43 -0500

    merge

--- public/js/main.js
+++ public/js/main.js
@@ -57734,61 +57734,66 @@ var LevelUp = function (_React$Component) {
 		}
 
 		return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = LevelUp.__proto__ || Object.getPrototypeOf(LevelUp)).call.apply(_ref, [this].concat(args))), _this), _this.state = { percent: '', nextLevel: '' }, _this.getNextLevelPoints = function (user) {
+			if (user.level == 0) pointsNeeded = 0;
+			if (user.level == 1) pointsNeeded = 4;
+			if (user.level == 2) pointsNeeded = 7;
+			if (user.level == 3) pointsNeeded = 9;
+			if (user.level == 4) pointsNeeded = 12;
+			if (user.level == 5) pointsNeeded = 15;
+			if (user.level == 6) pointsNeeded = 19;
+			if (user.level == 7) pointsNeeded = 23;
+			if (user.level == 8) pointsNeeded = 27;
+			if (user.level == 9) pointsNeeded = 32;
+			if (user.level == 10) pointsNeeded = 37;
+			if (user.level == 11) pointsNeeded = 43;
+			if (user.level == 12) pointsNeeded = 49;
+			if (user.level == 13) pointsNeeded = 56;
+			if (user.level == 14) pointsNeeded = 64;
+			if (user.level == 15) pointsNeeded = 72;
+			if (user.level == 16) pointsNeeded = 81;
+			if (user.level == 17) pointsNeeded = 91;
+			if (user.level == 18) pointsNeeded = 102;
+			if (user.level == 19) pointsNeeded = 115;
+			if (user.level == 20) pointsNeeded = 128;
+			if (user.level == 21) pointsNeeded = 143;
+			if (user.level == 22) pointsNeeded = 159;
+			if (user.level == 23) pointsNeeded = 177;
+			if (user.level == 24) pointsNeeded = 193;
+			if (user.level == 25) pointsNeeded = 211;
+			if (user.level == 26) pointsNeeded = 229;
+			if (user.level == 27) pointsNeeded = 250;
+			if (user.level == 28) pointsNeeded = 272;
+			if (user.level == 29) pointsNeeded = 296;
+			if (user.level == 30) pointsNeeded = 312;
+			if (user.level == 31) pointsNeeded = 330;
+			if (user.level == 32) pointsNeeded = 348;
+			if (user.level == 33) pointsNeeded = 368;
+			if (user.level == 34) pointsNeeded = 388;
+			if (user.level == 35) pointsNeeded = 410;
+			if (user.level == 36) pointsNeeded = 432;
+			if (user.level == 37) pointsNeeded = 456;
+			if (user.level == 38) pointsNeeded = 481;
+			if (user.level == 39) pointsNeeded = 507;
+			if (user.level == 40) pointsNeeded = 534;
+			if (user.level == 41) pointsNeeded = 563;
+			if (user.level == 42) pointsNeeded = 593;
+			if (user.level == 43) pointsNeeded = 624;
+			if (user.level == 44) pointsNeeded = 658;
+			if (user.level == 45) pointsNeeded = 692;
+			if (user.level == 46) pointsNeeded = 729;
+			if (user.level == 47) pointsNeeded = 768;
+			if (user.level == 48) pointsNeeded = 808;
+			if (user.level == 49) pointsNeeded = 850;
+			if (user.level == 50) pointsNeeded = 0;
 
-			var points = user.points || 0;
-			var level = 0,
-			    title = 'Apprentice',
-			    pointsNeeded = 1,
-			    nextTitle = 'Bronze Acolyte';
-			if (points > 0) {
-				level = 1;title = 'Bronze Acolyte', pointsNeeded = 12, nextTitle = 'Silver Acolyte';
-			}
-			if (points >= 12) {
-				level = 5;title = 'Silver Acolyte', pointsNeeded = 27, nextTitle = 'Gold Acolyte';
-			}
-			if (points >= 27) {
-				level = 9;title = 'Gold Acolyte', pointsNeeded = 37, nextTitle = 'Bronze Aspirant';
-			}
-			if (points >= 37) {
-				level = 11;title = 'Bronze Aspirant', pointsNeeded = 54, nextTitle = 'Silver Aspirant';
-			}
-			if (points >= 54) {
-				level = 15;title = 'Silver Aspirant', pointsNeeded = 102, nextTitle = 'Gold Aspirant';
-			}
-			if (points >= 102) {
-				level = 19;title = 'Gold Aspirant', pointsNeeded = 128, nextTitle = 'Bronze Adventurer';
-			}
-			if (points >= 128) {
-				level = 21;title = 'Bronze Adventurer', pointsNeeded = 193, nextTitle = 'Silver Adventurer';
-			}
-			if (points >= 193) {
-				level = 25;title = 'Silver Adventurer', pointsNeeded = 272, nextTitle = 'Gold Adventurer';
-			}
-			if (points >= 272) {
-				level = 29;title = 'Gold Adventurer', pointsNeeded = 312, nextTitle = 'Bronze Veteran';
-			}
-			if (points >= 312) {
-				level = 31;title = 'Bronze Veteran', pointsNeeded = 388, nextTitle = 'Silver Veteran';
-			}
-			if (points >= 388) {
-				level = 35;title = 'Silver Veteran', pointsNeeded = 481, nextTitle = 'Gold Veteran';
-			}
-			if (points >= 481) {
-				level = 39;title = 'Gold Veteran', pointsNeeded = 534, nextTitle = 'Bronze Master';
-			}
-			if (points >= 534) {
-				level = 41;title = 'Bronze Master', pointsNeeded = 658, nextTitle = 'Silver Master';
-			}
-			if (points >= 658) {
-				level = 45;title = 'Silver Master', pointsNeeded = 808, nextTitle = 'Gold Master';
-			}
-			if (points >= 808) {
-				level = 49;title = 'Gold Master', pointsNeeded = 0, nextTitle = '';
+			if (pointsNeeded > 0) {
+				var percentNeeded = (pointsNeeded - user.points) / 100;
+				var percent = 100 - percentNeeded * 100;
+			} else {
+				var percent = 99.9;
 			}
 
-			var percentNeeded = (pointsNeeded - user.points) / 100;
-			var percent = 100 - percentNeeded * 100;
-			_this.setState({ percent: percent, nextLevel: nextTitle });
+			_this.setState({ percent: percent, nextLevel: user.level + 1 });
 		}, _temp), _possibleConstructorReturn(_this, _ret);
 	}
 
